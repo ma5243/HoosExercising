074d5491f9812b7822a1f20af750df69942d8c03
Ankit Agrawal <aa6eu@virginia.edu>
2021-04-12 11:01:17 -0400

    Added tests to Spotify app

--- spotify/tests.py
+++ spotify/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
+from views import obtain_spotify_object, retrieve_song, retrieve_top_hits
+
 # Create your tests here.
+
+class SpotifyTests(TestCase):
+    client_id = '686f63c1a0564bfda4f0c9165efa21ef'
+    client_secret = '2477e715839840b4b6e42749d22bbbdc'
+
+    def test_obtain_spotify_object(self):
+        # Test that we obtain a non-null Spotipy object
+        obj = obtain_spotify_object(self.client_id, self.client_secret)
+        self.assertIsNotNone(obj)
+
+    def test_retrieve_top_hits(self):
+        # Test that Spotify retrieves the correct playlist
+        obj = obtain_spotify_object(self.client_id, self.client_secret)
+        top_hits = retrieve_top_hits(obj)
+        self.assertEqual(top_hits['name'], "Today's Top Hits")  
+
+    def test_retrieve_song_correct_title(self):
+        obj = obtain_spotify_object(self.client_id, self.client_secret)
+        song_title = "Juicy"
+        song_artist = "Doja Cat"
+        test_song = retrieve_song(obj, song_title, song_artist)
+        self.assertEqual(test_song['title'], song_title)  
+
+    def test_retrieve_song_correct_artist(self):
+        obj = obtain_spotify_object(self.client_id, self.client_secret)
+        song_title = "Juicy"
+        song_artist = "Doja Cat"
+        test_song = retrieve_song(obj, song_title, song_artist)
+        self.assertEqual(test_song['artist'], song_artist) 
+
+###############################
+# SOURCE: https://intellipaat.com/community/9924/call-a-function-from-another-file-in-python#:~:text=If%20you%20want%20to%20call,b)and%20you%20are%20done.
+###############################
