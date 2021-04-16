3452b7d90a5fb00e5c9bb575b1ce7f3ed1f704cb
Muhammad Abdullah <abd07142011@gmail.com>
2021-04-16 16:36:55 -0400

    tryna fix url

--- oauth/urls.py
+++ oauth/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.self_profile, name='profile'),
-    path('<int:profile_id>/', views.specific_profile, name='specific_profile'),
-    path('edit/', views.edit_or_redirect, name='edit_profile'),
+    path('profile/', views.self_profile, name='profile'),
+    path('profile/<int:profile_id>/', views.specific_profile, name='specific_profile'),
+    path('profile/edit/', views.edit_or_redirect, name='edit_profile'),
 ]
