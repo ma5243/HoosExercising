95a1b88eb58a326ea44298c8b71beada8572cd68
jcwiler88 <jcw5cf@virginia.edu>
2021-05-06 06:52:44 -0400

    basic models for team uploaded

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#model for actual team structure. includes team name and list of team members.
+class Team(models.Model):
+    name = models.charField(max_length = 75)
+    team_description = models.textField(max_length = 1000)
+    team_members = models.ManyToManyField(User, symmetrical=True, verbose_name="Team Members", null=True)
+
+    def __str__(self):
+        return self.name
+
+#posts to the team page.
+class Posts(models.Model):
+    title = models.charField(max_length = 50)
+    post_body = models.textField(max_length = 1000)
+
+    def __str__(self):
+        return self.title
+
+    
--- teams/views.py
+++ teams/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 #this is the section where you actually write what the code will display.
+
+#okay, here is the goal- have a tab titled teams, where people who belong to teams can see what teams 
+#they belong to. In each team's home page they have the list of all the team posts that are there.
+#i don't care who can post to the team pages it should just be able to happen.
